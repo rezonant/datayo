f1830bfa81675054131b2b48074dd12569a8e24b
William Lahti <wilahti@gmail.com>
2021-08-22 17:46:07 -0700

    test results: correct location

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
 import 'source-map-support/register';
 import { suite, ConsoleReporter, JUnitXMLReporter } from 'razmin';
+import * as path from 'path';
 
 suite()
     .withOptions({
         reporting: {
             reporters: [
                 new ConsoleReporter(),
-                new JUnitXMLReporter('test-results/razmin')
+                new JUnitXMLReporter(path.join(process.cwd(), 'test-results', 'razmin', 'test-results.xml'))
             ]
         }
     })
